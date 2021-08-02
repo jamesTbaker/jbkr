cfed964b56307e60b83bc0ea62678f65b54f9dec
James Baker <hi@jbkr.me>
2021-08-02 13:58:54 -0400

    Strapi-Cloudinary integration

---
+++ strapi/config/plugins.js
@@ -0,0 +1,12 @@
+module.exports = ({ env }) => ({
+	// ...
+	upload: {
+		provider: 'cloudinary',
+		providerOptions: {
+			cloud_name: env('cloudinaryCDNName'),
+			api_key: env('cloudinaryCDNKey'),
+			api_secret: env('cloudinaryCDNSecret'),
+		},
+	},
+	// ...
+});
