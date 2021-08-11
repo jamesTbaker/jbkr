5e60c68aea0b29f15153d1672ba31d9a5b2c5467
James Baker <hi@jbkr.me>
2021-08-11 15:14:24 -0400

    Save

--- doc/src/pages/index.js
+++ doc/src/pages/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import Layout from '@theme/Layout';
 import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
 import HomepageFeatures from '../components/HomepageFeatures';
 import siteConfigImport from '../../docusaurus.config.js';
 import styled from 'styled-components';
@@ -10,8 +9,28 @@ import styled from 'styled-components';
 
 
 const homeContent = {
-	'title': siteConfigImport.title,
+	'titlePreface': 'Welcome to the ',
+	'titleMain': 'Doc Party',
 	'tagline': '',
+	'taglineURL': 'https://en.wikipedia.org/wiki/Software_documentation',
+	'what': {
+		'header': 'About',
+		'body': 'jbkr is the core of James Baker\'s \
+			professional online presence.',
+	},
+	'stack': {
+		'header': 'Tech Stack Overview',
+		'body': [
+			'Figma: Colors are defined in Figma and fetched from its API.',
+			'ES6 + TypeScript: Styles and other modules.',
+			'Strapi: Content.',
+			'Next: Client.',
+			// '',
+			// '',
+			// '',
+			// '',
+		],
+	},
 };
 
 const HeaderContainer = styled.header`
@@ -20,46 +39,60 @@ const HeaderContainer = styled.header`
 	background-position: center bottom;
 	background-repeat: no-repeat;
 	background-color: #f00;
-	padding: 16rem 0 8rem;
+	padding: 18rem 0 6rem;
 `;
 const HeaderContentContainer = styled.div`
 	display: inline-block;
-	padding: 2rem 2rem 2rem 10rem;
+	padding: 2rem 4rem 2rem 6rem;
 	border-radius: 0 3px 3px 0;
-	background-color: hsla(225, 25%, 26%, .95);
+	background-color: hsla(275, 100%, 50%, .85);
 `;
 const HeaderTitle = styled.h1`
-	font-size 5.625rem;
+	font-size: 5.675rem;
 	line-height: 6rem;
-	font-weight: 700;
+	font-weight: 600;
 	color: white;
 `;
-const HeaderTagline = styled.p`
-	font-size 2rem;
-	line-height: 3rem;
+const HeaderTitlePreface = styled.div`
+	font-size 1rem;
+	line-height: 1.5rem;
 	font-weight: 400;
-	color: hsla(175, 100%, 50%);
+	color: hsla(190, 100%, 85%);
+`;
+const HeaderEmphasisLine = styled.div`
+	height: .25rem;
+	border-radius: 1px;
+	background-color: hsla(325, 100%, 50%, 1);
+`;
+const HeaderTagline = styled.p`
+	font-size 1rem;
+	line-height: 1.5rem;
+	padding-top: 1rem;
+	font-weight: 400;
+	color: hsla(190, 100%, 85%);
 `;
 const HeaderTaglineLink = styled.a`
 	color: white;
 `;
-const HomepageHeader = () => {
-	const { siteConfig } = useDocusaurusContext();
-	return (
-		<HeaderContainer>
-			<HeaderContentContainer>
-				<HeaderTitle>{siteConfig.title}</HeaderTitle>
-				<HeaderTagline>
-					Because who doesn't love&nbsp;
-					<HeaderTaglineLink
-						href=
-						"https://en.wikipedia.org/wiki/Software_documentation"
-						target="_blank">documentation</HeaderTaglineLink>?
-				</HeaderTagline>
-			</HeaderContentContainer>
-		</HeaderContainer>
-	);
-};
+const HomepageHeader = () => (
+	<HeaderContainer>
+		<HeaderContentContainer>
+			<HeaderTitle>
+				<HeaderTitlePreface>
+					{homeContent.titlePreface}
+				</HeaderTitlePreface>
+				{homeContent.titleMain}
+			</HeaderTitle>
+			<HeaderEmphasisLine />
+			<HeaderTagline>
+				Because who doesn't love&nbsp;
+				<HeaderTaglineLink
+					href={homeContent.taglineURL}
+					target="_blank">documentation</HeaderTaglineLink>?
+			</HeaderTagline>
+		</HeaderContentContainer>
+	</HeaderContainer>
+);
 
 export default () => {
 	const { siteConfig } = useDocusaurusContext();
@@ -70,8 +103,9 @@ export default () => {
 		>
 			<HomepageHeader />
 			<main>
-				<HomepageFeatures />
+				<h2>{homeContent.stack.header}</h2>
 			</main>
 		</Layout>
 	);
 };
+// <HomepageFeatures />
