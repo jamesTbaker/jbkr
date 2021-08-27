a91c773434e4cc6de89a2da69a2c83f8afe28d0a
James Baker <hi@jbkr.me>
2021-08-27 12:16:10 -0400

    Favicons

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -35,34 +35,32 @@ export default function App({ Component, pageProps }) {
 	return (
 		<>
 			<Head>
+				<meta name="author" content="James T. Baker" />
 				<meta
 					name="viewport"
 					content="initial-scale=1.0, width=device-width"
 				/>
-				<meta name="author" content="James T. Baker" />
 				<link rel="preconnect" href="https://fonts.googleapis.com" />
 				<link rel="preconnect"
-					href="https://fonts.gstatic.com" crossOrigin />
+					href="https://fonts.gstatic.com" crossOrigin="true" />
 				<link rel="stylesheet"
+					// eslint-disable-next-line max-len
 					href="https://fonts.googleapis.com/css2?family=Inter&display=swap"
 				/>
-
-
-				<link rel="apple-touch-icon" sizes="180x180"
-					href="/favicon/apple-touch-icon.png" />
-				<link rel="icon" type="image/png" sizes="32x32"
-					href="/favicon/favicon-32x32.png" />
-				<link rel="icon" type="image/png" sizes="194x194"
-					href="/favicon/favicon-194x194.png" />
-				<link rel="icon" type="image/png" sizes="192x192"
-					href="/favicon/android-chrome-192x192.png" />
+				<link rel="icon"
+					href="/favicon/brands.svg" type="image/svg+xml" />
 				<link rel="icon" type="image/png" sizes="16x16"
-					href="/favicon/favicon-16x16.png" />
+					href="/favicon/16px.png" />
+				<link rel="icon" type="image/png" sizes="32x32"
+					href="/favicon/32px.png" />
+				<link rel="apple-touch-icon" sizes="180x180"
+					href="/favicon/180px.png" />
+				<link rel="icon" type="image/png" sizes="192x192"
+					href="/favicon/192px.png" />
+				<link rel="icon" type="image/png" sizes="194x194"
+					href="/favicon/194px.png" />
 				<link rel="manifest" href="/favicon/site.webmanifest" />
-				<link rel="mask-icon" href="/favicon/safari-pinned-tab.svg?v=1"
-					color="#FF0055" />
-				<meta name="msapplication-TileColor" content="#60709F" />
-				<meta name="theme-color" content="#60709F" />
+				<meta name="theme-color" content="#60709f" />
 
 
 			</Head>
