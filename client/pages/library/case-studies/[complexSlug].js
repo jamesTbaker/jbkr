544aca26a412d6b892c85b6563c75667e37232e9
James Baker <hi@jbkr.me>
2021-09-02 17:39:47 -0400

    Save

--- client/pages/library/case-studies/[complexSlug].js
+++ client/pages/library/case-studies/[complexSlug].js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 
@@ -23,8 +24,8 @@ const LibLabItemScreen = ({ screen }) => {
 
 	// console.log('ObjectID');
 	// console.log(ObjectID);
-	// console.log('screen');
-	// console.log(screen);
+	console.log('screen');
+	console.log(screen);
 	return (<div></div>);
 };
 
@@ -43,51 +44,30 @@ export async function getServerSideProps(context) {
 				'as': 'BriefStatements',
 			},
 		},
-		// {
-		// 	'$lookup':
-		// 	{
-		// 		'from': 'components_content_complex_screen_sections',
-		// 		'let': { 'sectionID': '$Section.ref' },
-		// 		'pipeline': [
-		// 			{ '$match': { '$expr': { '$eq': ['$_id', '$$sectionID'] } } },
-		// 		],
-		// 		'as': 'MatchedSections',
-		// 	},
-		// },
-		// {
-		// 	'$lookup':
-		// 	{
-		// 		'from': 'components_content_complex_screen_sections',
-		// 		'localField': 'Section.ref',
-		// 		'foreignField': '_id',
-		// 		'as': 'Sections',
-		// 	},
-		// },
-		// {
-		// 	'$lookup':
-		// 	{
-		// 		'from': 'components_content_brief_statements',
-		// 		'localField': 'Sections.BriefStatement.ref',
-		// 		'foreignField': '_id',
-		// 		'as': 'SectionBriefStatements',
-		// 	},
-		// },
+		{
+			'$lookup':
+			{
+				'from': 'upload_file',
+				'localField': 'MetaImage',
+				'foreignField': '_id',
+				'as': 'MetaImages',
+			},
+		},
 		{
 			'$project': {
 				'_id': 0,
+				'Featured': 1,
 				'Slug': 1,
 				'Title': 1,
+				'Subtitle': 1,
 				'MetaTitle': 1,
 				'MetaDescription': 1,
+				'MetaImages': 1,
 				'SocialDescription': 1,
-				'SnippetDescription': 1,
-				'Subtitle': 1,
+				'PublicationDate': 1,
 				'Tagline': 1,
-				'BriefStatement': 1,
+				'BriefStatements.Statement': 1,
 				'Section': 1,
-				'MatchedSections': 1,
-				// 'Sections': 1,
-				// 'SectionBriefStatements': 1,
 			},
 		},
 	]).toArray();
@@ -96,7 +76,6 @@ export async function getServerSideProps(context) {
 	screenDataRaw.Section.forEach((sectionObject) => {
 		screenSectionIDs.push(new ObjectID(sectionObject.ref));
 	});
-	// console.log(screenSectionIDs);
 	let sectionDataRaw = await db.collection('components_content_complex_screen_sections').aggregate([
 		{ '$match': { '_id': { '$in': screenSectionIDs } } },
 		{
@@ -140,9 +119,129 @@ export async function getServerSideProps(context) {
 	let mediaDataRaw = await db.collection('upload_file')
 		.find({ '_id': { '$in': mediaItemIDs } }).toArray();
 	mediaDataRaw = JSON.parse(JSON.stringify(mediaDataRaw));
+	const returnFormattedBriefStatements = ({ briefStatementsRaw }) => {
+		const briefStatementsFormatted = [];
+		if (
+			typeof (briefStatementsRaw) === 'object' &&
+			briefStatementsRaw.length > 1 &&
+			briefStatementsRaw[0]
+		) {
+			briefStatementsRaw.forEach((briefStatementRaw) => {
+				briefStatementsFormatted.push(briefStatementRaw.Statement);
+			});
+		}
+		return briefStatementsFormatted;
+	};
+	const sectionsFormatted = [];
+	sectionDataRaw.forEach((sectionObject) => {
+		const sectionFormatted = {
+			'_id': sectionObject._id,
+			'sectionID': sectionObject.SectionID,
+			'sectionTitle': returnHTMLFromMarkdown({
+				'content': sectionObject.SectionTitle,
+				'options': {
+					'removeEndCapTags': true,
+				},
+			}),
+			'sectionIntro': returnHTMLFromMarkdown({
+				'content': sectionObject.SectionIntro,
+			}),
+			'sectionQuote': returnHTMLFromMarkdown({
+				'content': sectionObject.SectionQuote,
+				'options': {
+					'removeEndCapTags': true,
+				},
+			}),
+		};
+		if (sectionObject.SectionBriefStatements) {
+			sectionFormatted.sectionBriefStatements = returnFormattedBriefStatements({
+				'briefStatementsRaw': sectionObject.SectionBriefStatements,
+			});
+		}
 
-
+		const SubsectionsThisSection = [];
+		sectionObject.Subsections.forEach((subsectionObject) => {
+			const subsectionFormatted = {
+				'subsectionID': subsectionObject.SubsectionID,
+				'subsectionGravity': subsectionObject.SubsectionGravity,
+				'subsectionMediaGravity':
+					subsectionObject.SubsectionMediaGravity,
+			};
+			if (subsectionObject.SubsectionTitle) {
+				subsectionFormatted.subsectionTitle = subsectionObject.SubsectionTitle;
+			}
+			if (subsectionObject.SubsectionText) {
+				subsectionFormatted.subsectionText = subsectionObject.SubsectionText;
+			}
+			if (subsectionObject.SubsectionMediaSVGArray) {
+				subsectionFormatted.subsectionMediaSVGArray = subsectionObject.SubsectionMediaSVGArray;
+			}
+			const mediaThisSubsection = [];
+			subsectionObject.SubsectionMedia.forEach((mediaReference) => {
+				mediaDataRaw.forEach((mediaItem) => {
+					if (mediaItem._id === mediaReference) {
+						mediaThisSubsection.push(mediaItem);
+					}
+				});
+			});
+			if (mediaThisSubsection.length > 0) {
+				subsectionFormatted.subsectionMedia = mediaThisSubsection;
+			}
+			SubsectionsThisSection.push(subsectionFormatted);
+		});
+		sectionFormatted.subsections = SubsectionsThisSection;
+		sectionsFormatted.push(sectionFormatted);
+	});
+	const screenFormatted = {
+		'featured': screenDataRaw.Featured,
+		'slug': screenDataRaw.Slug,
+		'title': screenDataRaw.Subtitle ?
+			returnHTMLFromMarkdown({
+				'content': `${screenDataRaw.Title.trim()}: ${screenDataRaw.Subtitle}`,
+				'options': {
+					'removeEndCapTags': true,
+				},
+			}) :
+			returnHTMLFromMarkdown({
+				'content': screenDataRaw.Title,
+				'options': {
+					'removeEndCapTags': true,
+				},
+			}),
+		'metaTitle': screenDataRaw.MetaTitle,
+		'metaDescription': screenDataRaw.MetaDescription,
+		'metaImage': {
+			'url': returnSocialImageCloudinaryURI({
+				'imagePublicID':
+					screenDataRaw.MetaImages[0].provider_metadata.public_id,
+				'imageExtension': screenDataRaw.MetaImages[0].ext,
+				'gravity': screenDataRaw.MetaImageGravity,
+			}),
+			'alternativeText': screenDataRaw.MetaImages[0].alternativeText,
+		},
+		'socialDescription': screenDataRaw.SocialDescription,
+		'publicationDate': new Date(screenDataRaw.PublicationDate)
+			.toLocaleDateString('en-US', {
+				'year': 'numeric',
+				'month': 'long',
+				'day': 'numeric',
+			}),
+		'sections': sectionsFormatted,
+	};
+	if (screenDataRaw.Tagline) {
+		screenFormatted.tagline = returnHTMLFromMarkdown({
+			'content': screenDataRaw.Tagline,
+			'options': {
+				'removeEndCapTags': true,
+			},
+		});
+	}
+	if (screenDataRaw.BriefStatements) {
+		screenFormatted.briefStatements = returnFormattedBriefStatements({
+			'briefStatementsRaw': screenDataRaw.BriefStatements,
+		});
+	}
 	return {
-		'props': { 'screen': sectionDataRaw },
+		'props': { 'screen': screenFormatted },
 	};
 }
