2a1ac1b52d3bbff8104aa3f7ece1b24dedf8c4ae
James Baker <hi@jbkr.me>
2021-09-24 18:02:32 -0400

    Save

--- doc/docs/content/Database.md
+++ doc/docs/content/Database.md
@@ -9,7 +9,7 @@ sidebar_position: 1
 <div class="generate-docs all-sections">
 
 <section class="generate-docs module">
-<div class="generate-docs module-name">
+<div class="generate-docs module-name"><span class="identifier"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path id="container" d="M0 0h24v24H0z"/><path  id="content" d="M15 2a4 4 0 0 1 3.464 6.001L23 8v2h-2v10a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V10H1V8l4.536.001A4 4 0 0 1 12 3.355 3.983 3.983 0 0 1 15 2zm-4 8H5v9h6v-9zm8 0h-6v9h6v-9zM9 4a2 2 0 0 0-.15 3.995L9 8h2V6a2 2 0 0 0-1.697-1.977l-.154-.018L9 4zm6 0a2 2 0 0 0-1.995 1.85L13 6v2h2a2 2 0 0 0 1.995-1.85L17 6a2 2 0 0 0-2-2z"/></svg></span>
 
 ## @jbkr/db-client
 
@@ -45,7 +45,11 @@ the MongoDB Atlas jbkr cluster.
 
 </div>
 
+<details><summary>
+
 #### Type Definitions
+
+</summary><div>
 
 ##### `MongoDBClient`
 A Node.js MongoDB client, from the official
@@ -60,7 +64,13 @@ A Node.js MongoDB client, from the official
 
 </div>
 
+</div></details>
+
+<details><summary>
+
 #### Parameters
+
+</summary><div>
 
 <div class="generate-docs function-parameters">
 
@@ -69,17 +79,25 @@ A Node.js MongoDB client, from the official
 | \{\} | `Object` | Destructured parameters |
 | \{\}.dbName | `Object` | Name of database to which to connect. |
 
-</div>
+</div></div></details>
+
+<details><summary>
 
 #### Return
+
+</summary><div>
 
 <p class="generate-docs function-return">
 
 Returns: `MongoDBClient` — A MongoDB Client connected to the specified database in the MongoDB Atlas jbkr cluster.
 
-</p>
+</p></div></details>
 
-#### Example(s)
+<details><summary>
+
+#### Examples
+
+</summary><div>
 
 <div class="generate-docs function-parameters">
 
@@ -92,7 +110,7 @@ const dbConnection = await returnDatabaseConnection({
 const data = await dbConnection.collection('collectionName').findOne({});
 ```
 
-</div>
+</div></div></details>
 
 <p class="generate-docs function-source-location">
 
@@ -119,7 +137,11 @@ the MongoDB Atlas jbkr cluster.
 
 </div>
 
+<details><summary>
+
 #### Type Definitions
+
+</summary><div>
 
 ##### `MongoDBClient`
 A Node.js MongoDB client, from the official
@@ -134,13 +156,19 @@ A Node.js MongoDB client, from the official
 
 </div>
 
+</div></details>
+
+<details><summary>
+
 #### Return
+
+</summary><div>
 
 <p class="generate-docs function-return">
 
 Returns: `MongoDBClient` — A MongoDB Client connected to the MongoDB Atlas jbkr cluster.
 
-</p>
+</p></div></details>
 
 <p class="generate-docs function-source-location">
 
--- modules/generate-docs/lib/generate.js
+++ modules/generate-docs/lib/generate.js
@@ -134,7 +134,8 @@ module.exports = {
 	},
 	'returnModuleContentMarkdown': ({ moduleObject }) => {
 		let moduleMarkdown = `<section class="generate-docs module">\n` +
-			`<div class="generate-docs module-name">\n\n` +
+			`<div class="generate-docs module-name">` +
+			`<span class="identifier"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path id="container" d="M0 0h24v24H0z"/><path  id="content" d="M15 2a4 4 0 0 1 3.464 6.001L23 8v2h-2v10a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V10H1V8l4.536.001A4 4 0 0 1 12 3.355 3.983 3.983 0 0 1 15 2zm-4 8H5v9h6v-9zm8 0h-6v9h6v-9zM9 4a2 2 0 0 0-.15 3.995L9 8h2V6a2 2 0 0 0-1.697-1.977l-.154-.018L9 4zm6 0a2 2 0 0 0-1.995 1.85L13 6v2h2a2 2 0 0 0 1.995-1.85L17 6a2 2 0 0 0-2-2z"/></svg></span>\n\n` +
 			`## ${moduleObject.name}\n\n</div>\n\n` +
 			`<div class="generate-docs module-description">\n` +
 			`${moduleObject.description}\n` +
@@ -211,10 +212,12 @@ module.exports = {
 		const typesThisFunction = types.filter((typeObject) =>
 			elementNames.includes(typeObject.name));
 		if (typesThisFunction && typesThisFunction[0]) {
-			functionMarkdown += `#### Type Definitions\n\n`;
+			functionMarkdown += `<details><summary>\n\n` +
+				`#### Type Definitions\n\n</summary><div>\n\n`;
 			functionMarkdown += module.exports.returnTypesContentMarkdown({
 				'types': typesThisFunction,
 			});
+			functionMarkdown += `</div></details>\n\n`;
 		}
 		if (
 			member.params &&
@@ -223,12 +226,14 @@ module.exports = {
 			member.params[0].type.names &&
 			member.params[0].type.names[0]
 		) {
-			functionMarkdown += `#### Parameters\n\n`;
+			functionMarkdown +=
+				`<details><summary>\n\n` +
+				`#### Parameters\n\n</summary><div>\n\n`;
 			functionMarkdown += module.exports
 				.returnElementsTableContentMarkdown({
 					'elements': member.params,
 				});
-			functionMarkdown += `\n\n`;
+			functionMarkdown += `</div></details>\n\n`;
 		}
 		if (
 			member.returns &&
@@ -237,23 +242,25 @@ module.exports = {
 			member.returns[0].type.names &&
 			member.returns[0].type.names[0]
 		) {
-			functionMarkdown += `#### Return\n\n`;
+			functionMarkdown += `<details><summary>\n\n` +
+				`#### Return\n\n</summary><div>\n\n`;
 			functionMarkdown += module.exports
 				.returnFunctionReturnContentMarkdown({
 					'returnObject': member.returns[0],
 				});
-			functionMarkdown += `\n\n`;
+			functionMarkdown += `</div></details>\n\n`;
 		}
 		if (
 			member.examples &&
 			member.examples[0]
 		) {
-			functionMarkdown += `#### Example(s)\n\n`;
+			functionMarkdown += `<details><summary>\n\n` +
+				`#### Examples\n\n</summary><div>\n\n`;
 			member.examples.forEach((exampleObject) => {
 				functionMarkdown += module.exports
 					.returnCodeExampleContentMarkdown({ exampleObject });
 			});
-			functionMarkdown += `\n\n`;
+			functionMarkdown += `</div></details>\n\n`;
 		}
 		functionMarkdown +=
 			`<p class="generate-docs function-source-location">\n\n` +
