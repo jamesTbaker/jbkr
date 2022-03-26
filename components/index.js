26a41b08db303481ed9a0a25310a591580128724
James Baker <hi@jbkr.me>
2022-03-26 15:16:24 -0400

    Save

--- components/index.js
+++ components/index.js
@@ -1,6 +1,71 @@
-export { ProfileScreen } from './lib/app/Screens/ProfileScreen.jsx';
-export { LibLabScreen } from './lib/app/Screens/LibLabScreen.jsx';
-export { ContactScreen } from './lib/app/Screens/ContactScreen.jsx';
+// Primitive
+export { Brand } from './lib/primitive/Brand/Brand.jsx';
+export { Icon, IconNames } from './lib/primitive/Icon/Icon.jsx';
+export { Line } from './lib/primitive/Line/Line.jsx';
+export { Text } from './lib/primitive/Text/Text.jsx';
+
+// Core
+export { Button } from './lib/core/Button/Button.jsx';
+export { Collapsible } from './lib/core/Collapsible/Collapsible.jsx';
+export { Copy } from './lib/core/Copy/Copy.jsx';
+export { CopyLink } from './lib/core/CopyLink/CopyLink.jsx';
+
+// App - Article
+export { Article } from './lib/app/Article/Article.jsx';
+export { ArticleSection } from './lib/app/Article/ArticleSection.jsx';
+export { ArticleSections } from './lib/app/Article/ArticleSections.jsx';
+export { ArticleSubsection } from './lib/app/Article/ArticleSubsection.jsx';
+export { ArticleUnifiedBody } from './lib/app/Article/ArticleUnifiedBody.jsx';
+
+// App - Common
+export { AppScaffold } from './lib/app/Common/AppScaffold.jsx';
+export { AppHeader } from './lib/app/Common/AppHeader.jsx';
+export { AppFooter } from './lib/app/Common/AppFooter.jsx';
+export { MediaItem } from './lib/app/Common/MediaItem.jsx';
+export { ScreenTitlePrimary } from './lib/app/Common/ScreenTitlePrimary.jsx';
+export { ScreenTitleSecondary } from './lib/app/Common/ScreenTitleSecondary.jsx';
+export { Spinner } from './lib/app/Common/Spinner.jsx';
+export { VideoItem } from './lib/app/Common/VideoItem.jsx';
+
+// App - Contact
+export { Contact } from './lib/app/Contact/Contact.jsx';
+
+// App - FourOhFour
+export { FourOhFour } from './lib/app/FourOhFour/FourOhFour.jsx';
+
+// App - Hub
+export { HubColorPreview } from './lib/app/Hub/HubColorPreview.jsx';
+export { HubContacts } from './lib/app/Hub/HubContacts.jsx';
+export { HubLogos } from './lib/app/Hub/HubLogos.jsx';
+export { HubLogotype } from './lib/app/Hub/HubLogotype.jsx';
+export { HubPrivate } from './lib/app/Hub/HubPrivate.jsx';
+
+// App - LibLab
+export { ArticleSummary } from './lib/app/LibLab/ArticleSummary.jsx';
+export { LibLab } from './lib/app/LibLab/LibLab.jsx';
+
+// App - Meta
+export { Meta } from './lib/app/Meta/Meta.jsx';
+
+// App - Profile
+export { Profile } from './lib/app/Profile/Profile.jsx';
+export { ProfileEducationCertification } from './lib/app/Profile/ProfileEducationCertification.jsx';
+export { ProfileProfessionalExperience } from './lib/app/Profile/ProfileProfessionalExperience.jsx';
+export { ProfileSection } from './lib/app/Profile/ProfileSection.jsx';
+export { ProfileSectionHeader } from './lib/app/Profile/ProfileSectionHeader.jsx';
+export { ProfileSkillRating } from './lib/app/Profile/ProfileSkillRating.jsx';
+export { PrProfileSkillVisualizationofile } from './lib/app/Profile/ProfileSkillVisualization.jsx';
+export { ProfileValuePropositionOne } from './lib/app/Profile/ProfileValuePropositionOne.jsx';
+export { ProfileValuePropositionTwo } from './lib/app/Profile/ProfileValuePropositionTwo.jsx';
+export { ProfileVolunteerExperience } from './lib/app/Profile/ProfileVolunteerExperience.jsx';
+
+
+
+
+// Screens
 export { ArticleScreen } from './lib/app/Screens/ArticleScreen.jsx';
-export { MetaScreen } from './lib/app/Screens/MetaScreen.jsx';
+export { ContactScreen } from './lib/app/Screens/ContactScreen.jsx';
 export { FourOhFourScreen } from './lib/app/Screens/FourOhFourScreen.jsx';
+export { LibLabScreen } from './lib/app/Screens/LibLabScreen.jsx';
+export { MetaScreen } from './lib/app/Screens/MetaScreen.jsx';
+export { ProfileScreen } from './lib/app/Screens/ProfileScreen.jsx';
