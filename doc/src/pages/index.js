264a601f9edf3b94c9256eb1018480ba7a525c6e
James Baker <hi@jbkr.me>
2022-04-02 18:18:04 -0400

    Save

--- doc/src/pages/index.js
+++ doc/src/pages/index.js
@@ -4,34 +4,6 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import styled from 'styled-components';
 import { deviceWidthQuery } from '@jbkr/style-service';
 
-const HeaderTagline = styled.p`
-	margin-bottom: 0;
-	padding: 0 0 2rem;
-	${deviceWidthQuery.only({ 'width': 'l' })} {
-		font-size: 1.375rem;
-		font-weight: 342;
-		font-style: normal;
-		line-height: 2rem;
-		letter-spacing: -.01033203125rem;
-	}
-	${deviceWidthQuery.only({ 'width': 'm' })} {
-		font-size: 1.4375rem;
-		font-weight: 342;
-		font-style: normal;
-		line-height: 2.5rem;
-		letter-spacing: -.009453125rem;
-	}
-	${deviceWidthQuery.only({ 'width': 's' })} {
-		font-size: 1.25rem;
-		font-weight: 342;
-		font-style: normal;
-		line-height: 2rem;
-		letter-spacing: -.0078125rem;
-	}
-`;
-const BodyContentContainer = styled.div`
-	padding: 0 2rem 2rem;
-`;
 export default () => {
 	const { siteConfig } = useDocusaurusContext();
 	return (
@@ -44,16 +16,16 @@ export default () => {
 					<h1>
 						Docs
 					</h1>
-					<HeaderTagline>
+					<p>
 						Welcome to the doc party. Because who doesn't love&nbsp;
 						<a
 							href="https://en.wikipedia.org/wiki/Software_documentation"
 							target="_blank">documentation
 						</a>?
-					</HeaderTagline>
+					</p>
 				</header>
 
-				<BodyContentContainer>
+				<div id="body-container">
 					<h2>About</h2>
 					<p>
 						<a href="https://jbkr.me">jbkr</a> is the core
@@ -89,7 +61,7 @@ export default () => {
 
 						</li> */}
 					</ul>
-				</BodyContentContainer>
+				</div>
 			</main>
 		</Layout>
 	);
