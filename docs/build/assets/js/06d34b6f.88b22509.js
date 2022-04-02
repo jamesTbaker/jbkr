ee61cd48875b936ec102e04ad321c2967f11a030
James Baker <hi@jbkr.me>
2022-04-02 15:41:09 -0400

    Save

---
+++ docs/build/assets/js/06d34b6f.88b22509.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_jbkr_doc=self.webpackChunk_jbkr_doc||[]).push([[73],{3769:function(u){u.exports=JSON.parse('{"plugin":{"name":"docusaurus-plugin-content-docs","id":"default"}}')}}]);
---
+++ docs/build/assets/js/f0b5eec2.a2e180b3.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk_jbkr_doc=self.webpackChunk_jbkr_doc||[]).push([[358],{5745:function(e){e.exports=JSON.parse('{"plugin":{"name":"docusaurus-plugin-content-pages","id":"default"}}')}}]);
